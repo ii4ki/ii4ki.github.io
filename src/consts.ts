920e6f7a82dc3d75ad7470da7a5b013a3dd29c58
Tony <lightning_tony@protonmail.com>
2026-05-16 09:10:06 +0400

    fix: caps title

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
-export const SITE_TITLE = 'ii4ki';
+export const SITE_TITLE = 'II4KI';
 export const SITE_DESCRIPTION = 'vibe coder. automations specialist. developer. builder of small tools that do one thing well.';
